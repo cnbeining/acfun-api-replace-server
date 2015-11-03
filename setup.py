c4772887ac97ec1d508dff10ad030e0027a9afeb
David Zhuang <cnbeining@gmail.com>
2015-11-03 01:43:52 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(name='openshiftwebpy',
       version='1.0',
-      description='OpenShift web.py example',
+      description='OpenShift web.py API',
       author='Ramiro Gomez',
-      author_email='www@ramiro.org',
+      author_email='i@cnbeining.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['web.py', 'Jinja2']
+      install_requires=['web.py', 'Jinja2', 'requests']
      )

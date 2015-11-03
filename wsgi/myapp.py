e1c0abcbd37a4a0cb324254efd8f6428fa9c9f6e
David Zhuang <cnbeining@gmail.com>
2015-11-03 01:43:02 -0500

    Update myapp.py

--- wsgi/myapp.py
+++ wsgi/myapp.py
@@ -1,30 +1,45 @@
-import os
+#!/usr/bin/env python
+#coding:utf-8
+# Author:  Beining --<i#cnbeining.com>
+# Purpose: Replacement of Acfun's API
+# Created: 11/03/2015
+
 import web
-from jinja2 import Environment,FileSystemLoader
+import requests
+import sys
 
-urls = ('/', 'hello')
-app = web.application(urls, globals())
-render = web.template.render('templates/')
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
+urls = (
+    '/getVideo', 'getVideo'
+)
 
-def render_template(template_name, **context):
-    extensions = context.pop('extensions', [])
-    globals = context.pop('globals', {})
-
-    jinja_env = Environment(
-            loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
-            extensions=extensions,
-            )
-    jinja_env.globals.update(globals)
-
-    #jinja_env.update_template_context(context)
-    return jinja_env.get_template(template_name).render(context)
-
-
-class hello:
+class getVideo:
     def GET(self):
-        web.header('Content-Type', 'text/html')
-        return render_template('hello.html')
+        user_data = web.input()
+        try:  #anti BS
+            id = int(user_data.id)
+        except ValueError, AttributeError:
+            return 'ERROR'
+        
+        api_url = 'http://www.acfun.tv/video/getVideo.aspx?id={id}'.format(id = id)
+        
+        try:
+            res = requests.get(api_url)
+            res = res.json()
+        except Exception as e:
+            return 'ERROR'
+        
+        if not res['success']:  #failed
+            return res.replace("u'", "'").replace("'", '"').replace('True', 'true')  #throw the shit back
+        
+        if str(res['sourceType']) == 'zhuzhan':  #compressed shit
+            del(res['videoList'])  #strip this
+            res['sourceType'] = 'letv'  #force change back
+            
+        return str(res).replace("u'", "'").replace("'", '"').replace('True', 'true')
 
-
+app = web.application(urls, globals())
+#app.run()
 application = app.wsgifunc()
